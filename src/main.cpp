25434470c91576752b979f6056791b79da45283b
Hal Crossno <hal@workethicrecords.com>
2021-09-26 13:41:58 -0500

    adding all files to this test branch. Not 100% sure if we'll be needing all of this

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//press F7 to compile in Sublime Text <- so Hal remembers what the key is
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+int main(int argc, char* args[])
+{
+	std::cout << "hello earth" << std::endl;
+	std::cin.get();
+	return 0;
+	
+}
