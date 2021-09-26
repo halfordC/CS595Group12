a6c688bc64bc9bf1764e1c0731fa48e3501ef58e
Hal Crossno <hal@workethicrecords.com>
2021-09-26 15:11:15 -0500

    Simple rendering system, blank window pops up and hangs out

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,42 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 
+#include "RenderWindow.hpp"
+
+bool programRunning = true;
+
 int main(int argc, char* args[])
 {
-	std::cout << "hello earth" << std::endl;
-	std::cin.get();
+	if(SDL_Init(SDL_INIT_VIDEO) > 0)
+	{
+		std::cout << "SDL_Init has Failed. SDL_ERROR:  " << SDL_GetError() << std::endl;
+	}
+
+	if(!(IMG_Init(IMG_INIT_PNG)))
+	{
+		std::cout << "IMG_Init has Failed. SDL_ERROR:  " << SDL_GetError() << std::endl;
+	}
+
+	RenderWindow window("FirstWindow", 640, 480);
+
+	SDL_Event event;
+
+	while(programRunning)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+			{
+				programRunning = false;
+			}
+
+		}
+
+	} 
+	
+	window.cleanUp();
+	SDL_Quit();
+
 	return 0;
 	
 }
