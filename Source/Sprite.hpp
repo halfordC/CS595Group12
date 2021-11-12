63e3db4b9eb46566411aa1c661d69fc37270389f
qwalters4 <45807765+qwalters4@users.noreply.github.com>
2021-11-12 15:49:43 -0600

    merged

--- Source/Sprite.hpp
+++ Source/Sprite.hpp
@@ -11,18 +11,14 @@ public:
     y = 0.0f;
     w = 0;
     h = 0;
-    xV = 1.0f;
-    yV = 1.0f;
     res = NULL;
   }
-  Sprite(float xPos, float yPos, int width, int height, SDL_Texture* resource, int r1)
+  Sprite(float xPos, float yPos, int width, int height, SDL_Texture* resource)
   {
     x = xPos;
     y = yPos;
     w = width;
     h = height;
-    xV = (r1 == 0) ? 1.0f : -1.0f;
-    yV = (r1 == 0) ? 1.0f : -1.0f;
     rot = 0;
     scale = 1.0f;
     res = resource;
@@ -35,17 +31,6 @@ public:
   void setScale(float s){ if(s > 0) { scale = s; } };
   float getX() { return x; };
   float getY() { return y; };
-  void update() 
-  { 
-      
-      y += (0.01f * yV);
-      x += (0.01f * xV);
-      rot += 10;
-      if (y + (0.01f * yV) >= 1.0f) yV = -1;
-      if (y + (0.01f * yV) < 0.0f) yV = 1;
-      if (x + (0.01f * xV) >= 1.0f) xV = -1;
-      if (x + (0.01f * xV) < 0.0f) xV = 1;
-  };
   int getWidth() { return w; };
   int getHeight() { return h; };
   int getRotation() { return rot; };
@@ -58,8 +43,6 @@ private:
   float y;
   int w;
   int h;
-  float xV;
-  float yV;
   int rot;
   float scale;
   SDL_Texture* res;
