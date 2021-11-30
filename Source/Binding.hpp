64deb2aa5e9abeb1337ae69233653a4f331212cf
qwalters4 <qwalters4@gmail.com>
2021-11-30 13:47:18 -0600

    fixed build errors

--- Source/Binding.hpp
+++ Source/Binding.hpp
@@ -13,7 +13,7 @@ public:
         type = 0;
         amount = 0.0f;
     }
-    Binding(string path, int trigger, int target, int type, float amount)
+    Binding(std::string path, int trigger, int target, int type, float amount)
     {
         path = path;
         trigger = trigger;
@@ -22,17 +22,17 @@ public:
         amount = amount;
     };
     void setTrigger(float trigger) { trigger = trigger; };
-    void setPath(string path) { path = path; };
+    void setPath(std::string path) { path = path; };
     void setTarget(float target) { target = target; };
     void setType(int type) { type = type; };
     void setAmount(float amount) { amount = amount; };
     int getTrigger() { return trigger; };
-    string getPath() { return path; };
+    std::string getPath() { return path; };
     int getTarget() { return target; };
     int getType() { return type; };
     float getAmount() { return amount; };
 private:
-    string path; //path for the binding
+    std::string path; //path for the binding
     int trigger; //Trigger note
     int target; //Which property to target: 0=x, 1=y, 2=width, 3=heigt, 4=rotation, 5=size
     int type; //Type of change: 0=Set, 1=Scale
