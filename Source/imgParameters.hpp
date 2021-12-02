7c24caf96650fbdbc06e04ad573ec253393e4d1a
grantjadams <gjadams@uwm.edu>
2021-12-01 21:25:32 -0600

    Progress on imgParameters

--- Source/imgParameters.hpp
+++ Source/imgParameters.hpp
@@ -13,7 +13,27 @@
 class imgParameters
 {
 public:
-	imgParameters();
-private:
+	imgParameters(myKissGUI* kissGUI);
 
+	void render();
+	void cleanUp();
+	void selectMidiParamEvent(SDL_Event* e);
+	void selectImageParamEvent(SDL_Event* e);
+	void typeFilePath(SDL_Event* e);
+	void midiLearnEvent(SDL_Event* e);
+	void midiListenButton(SDL_Event* e, MidiModule* myMidiModule);
+	void browseEvent(SDL_Event* e, RenderWindow myRenderWindow);
+
+	kiss_window binding;
+	kiss_combobox imgParam;
+	kiss_combobox midiParam;
+	kiss_entry noteEntry;
+	kiss_entry filePathEntry;
+	kiss_button browsePath;
+	kiss_button midiLearn;
+	kiss_array midiParamList;
+	kiss_array imageParamList;
+
+private:
+	
 };
