7a77cece3ba183e70d6fb4e19f19ce5672782771
grantjadams <gjadams@uwm.edu>
2021-12-01 20:12:22 -0600

    imgParameters class

---
+++ Source/imgParameters.cpp
@@ -0,0 +1,19 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <vector>
+
+
+#include "UserGUI.hpp"
+//#include "kiss_sdl.h"
+#include "myKissGui.hpp"
+#include "midiModule.h"
+#include "RenderWindow.hpp"
+#include "imgParameters.hpp"
+
+imgParameters::imgParameters()
+{
+
+}
