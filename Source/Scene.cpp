daf151d83ab7a4186fe9fd5b1f28dcfa895fc3b9
Hal Crossno <hal@workethicrecords.com>
2021-12-01 20:47:16 -0600

    added simple rendering system to Scene Class

--- Source/Scene.cpp
+++ Source/Scene.cpp
@@ -12,11 +12,38 @@
 #include "midiModule.h"
 #include "RenderWindow.hpp"
 #include "Scene.h"
+#include "imgParameters.hpp"
 
 using std::cout; using std::cin;
 using std::endl; using std::string;
 
 Scene::Scene() 
 {
+	imgParamIndex = 0; //this is the number of imgParam objects in our ImgParam array.
+	imgParameters *firstIMPar = new imgParameters();
+	imgParArray[imgParamIndex] = firstIMPar;
+	imgParamIndex++;
+
+
+}
+void Scene::addImg(myKissGUI *kissGUI, SDL_Event* e)
+{
+	int draw = 1;
+	if (kissGUI->kiss_button_event(&addImgButton, e, &draw))
+	{
+		imgParameters* nextIMPar = new imgParameters();
+		imgParArray[imgParamIndex] = nextIMPar;
+		imgParamIndex++;
+	}
+}
+
+void Scene::render(int *draw, SDL_Renderer* renderer, myKissGUI *kissGUI)
+{
+	SDL_RenderClear(renderer);
+	kissGUI->kiss_window_draw(&sceneWindow, renderer);
+
+
+
+	//also, call the render function on all img that we have in scene view. 
 
 }
--- Source/Scene.h
+++ Source/Scene.h
@@ -8,12 +8,21 @@
 #include "myKissGui.hpp"
 #include "midiModule.h"
 #include "RenderWindow.hpp"
+#include "imgParameters.hpp"
 
 
 class Scene 
 {
 public:
 	Scene();
-private:
+	imgParameters * imgParArray[15]; // setting a hard limit of 15 images for now. Might change later.
 
+	void addImg(myKissGUI* kissGUI, SDL_Event *e);
+	void render(int * draw, SDL_Renderer* renderer, myKissGUI* kissGUI);
+
+	kiss_button addImgButton;
+	kiss_window sceneWindow;
+	//we need a function to add 
+private:
+	int imgParamIndex; //should not go over 15. 
 };
