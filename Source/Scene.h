f954dec41fd7647268331918ac0e3529a0f2267a
Hal Crossno <hal@workethicrecords.com>
2021-12-01 20:03:07 -0600

    added Scene class cpp and header

---
+++ Source/Scene.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include "myKissGui.hpp"
+#include "midiModule.h"
+#include "RenderWindow.hpp"
+
+
+class Scene 
+{
+public:
+	Scene();
+private:
+
+};
