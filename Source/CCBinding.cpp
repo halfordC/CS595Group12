b31edddcbf911e8d98cac2af428680b41b58e98d
Hal Crossno <hal@workethicrecords.com>
2021-12-06 19:50:59 -0600

    CCBindings Class Implemented

---
+++ Source/CCBinding.cpp
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <iostream>
+#include <string>
+
+#include "CCBinding.h"
+
+CCBinding::CCBinding() 
+{
+	param = 6; //not a valid param, essentially null
+	CCnumber = 0;
+	CCChannel = 0;
+}
